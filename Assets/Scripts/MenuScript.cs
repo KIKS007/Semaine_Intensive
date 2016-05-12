7149e3f9d10b85cae7e87ee8be4a7ed1e9ff38cb
Kazura23 <tom.kcz@hotmail.fr>
2016-05-12 13:10:26 +0200

    MENU ajouté

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MenuScript : MonoBehaviour 
+{
+
+
+	public void QuitGame ()
+	{
+		Application.Quit();
+
+	}
+
+	public void PlayGame ()
+	{
+		SceneManager.LoadScene("Main");
+	}
+		
+}
